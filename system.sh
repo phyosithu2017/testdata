7855ca09072dfa92d00daefa3a42eb59afb9bd35
phyosithu2017 <kophyosithu.mm@gmail.com>
2021-12-09 08:33:27 +0000

    This script will show linux information.

---
+++ system.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+
+# Written by Phyo Sithu Maung at  Dec-9-2021.
+# ( This  is intened for education purpose only.) 
+
+
+# This commands will show you the kernel version of your linux.
+# You will see this will show to you the current kernel version of your 's linux system.
+
+echo "----------Kernel Version------------"
+
+echo "------------------------------------"
+uname -r
+echo "------------------------------------"
+
+echo  "--------Release Information--------"
+
+echo "------------------------------------"
+
+# This commmands will show  the release information of your Linux, Distrubition Names & Information, Support URL.
+
+cat /etc/*-release 
+
+echo "-----------------------------------"
